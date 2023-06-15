b9327a01eea61125c5c7ca6c4213f291fec2edb4
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2023-06-15 14:06:46 +0300

    useformikten Formik componentine geçiş yapıldı

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,8 +1,8 @@
 import Button from "../../components/Button"
 import { Link } from 'react-router-dom';
 import * as yup from 'yup';
-import { useFormik } from 'formik';
-import * as utils from '../../utils/index'
+import { useFormik, Formik, ErrorMessage, Field } from 'formik';
+//import * as utils from '../../utils/index'
 import styles from '../../Layouts/LoginRegisterLayout/loginregister.module.css';
 import { useAppDispatch } from "../../store/hook";
 import { loginUser } from "../../store/features/userSlice";
@@ -17,48 +17,82 @@ function Login() {
   const dispatch = useAppDispatch();
   //const { serverError } = useAppSelector(state => state.user);
 
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    }, validationSchema,
-    onSubmit: async (values, helpers) => {
-      const res = await dispatch(loginUser(values));
-      if (res.payload.error) {
-        helpers.setFieldError(res.payload.path, res.payload.msg);
-      }
-    },
-  });
+  // const formik = useFormik({
+  //   initialValues: {
+  //     email: '',
+  //     password: '',
+  //   }, validationSchema,
+  //   onSubmit: async (values, helpers) => {
+  //     const res = await dispatch(loginUser(values));
+  //     if (res.payload.error) {
+  //       helpers.setFieldError(res.payload.path, res.payload.msg);
+  //     }
+  //   },
+  // });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      {formik.isSubmitting && 'Giriş Yapılıyor...'}
-      <label htmlFor="email">Email</label>
-      <input
-        placeholder="Enter your mail"
-        type="email"
-        id="email"
-        name="email"
-        onBlur={formik.handleBlur}
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email && <div className={styles.error}>{formik.errors.email}</div>}
-      <label htmlFor="password">Password</label>
-      <input
-        placeholder="Enter your password"
-        type="password"
-        id="password"
-        name="password"
-        onBlur={formik.handleBlur}
-        onChange={formik.handleChange}
-        value={formik.values.password}
-      />
-      {formik.touched.password && formik.errors.password && <div className={styles.error}>{formik.errors.password}</div>}
-      <Button disabled={utils.isEmpty(formik.errors) ? false : true} type="submit" color="var(--default-blue)">Log In</Button>
-      <span>Don't have an account? <Link to='register'>Sign Up</Link></span>
-    </form>
+    <Formik
+      validationSchema={validationSchema}
+      initialValues={{
+        email: '',
+        password: ''
+      }} onSubmit={async (values, helpers) => {
+        const res = await dispatch(loginUser(values));
+        if (res.payload.error) {
+          helpers.setFieldError(res.payload.path, res.payload.msg);
+        }
+      }}
+    >
+      {({ isSubmitting, isValid, dirty, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          {isSubmitting && 'Giriş Yapılıyor...'}
+          <label htmlFor="email">Email</label>
+          <Field name='email' component='input' type='email' placeholder="Enter your mail" />
+          <ErrorMessage className={styles.error} name="email" component='small' />
+          <label htmlFor="password">Password</label>
+          <Field name='password' component='input' type='password' placeholder="Enter your password" />
+          <ErrorMessage className={styles.error} name="password" component='small' />
+          <Button disabled={!isValid} type="submit" color="var(--default-blue)">Log In</Button>
+          <span>Don't have an account? <Link to='register'>Sign Up</Link></span>
+        </form>
+      )}
+    </Formik>
   )
+
+
+  // return (
+  //   <form onSubmit={formik.handleSubmit}>
+  //     {formik.isSubmitting && 'Giriş Yapılıyor...'}
+  //     <label htmlFor="email">Email</label>
+  //     <input
+  //       placeholder="Enter your mail"
+  //       type="email"
+  //       id="email"
+  //       name="email"
+  //       onBlur={formik.handleBlur}
+  //       onChange={formik.handleChange}
+  //       value={formik.values.email}
+  //     />
+  //     {formik.touched.email && formik.errors.email && <div className={styles.error}>{formik.errors.email}</div>}
+  //     <label htmlFor="password">Password</label>
+  //     <input
+  //       placeholder="Enter your password"
+  //       type="password"
+  //       id="password"
+  //       name="password"
+  //       onBlur={formik.handleBlur}
+  //       onChange={formik.handleChange}
+  //       value={formik.values.password}
+  //     />
+  //     {formik.touched.password && formik.errors.password && <div className={styles.error}>{formik.errors.password}</div>}
+  //     {JSON.stringify({
+  //       valid: formik.isValid,
+  //       dirty: formik.dirty
+  //     })}
+  //     <Button disabled={formik.isValid || formik.dirty} type="submit" color="var(--default-blue)">Log In</Button>
+  //     <span>Don't have an account? <Link to='register'>Sign Up</Link></span>
+  //   </form>
+  // )
 }
 
 export default Login
--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,7 +1,7 @@
 import Button from "../../components/Button"
 import { Link } from 'react-router-dom';
 import * as yup from 'yup';
-import { useFormik } from 'formik';
+import { Formik, Field, ErrorMessage } from 'formik';
 import * as utils from '../../utils/index'
 import styles from '../../Layouts/LoginRegisterLayout/loginregister.module.css';
 import { useAppDispatch } from "../../store/hook";
@@ -17,71 +17,44 @@ const validationSchema = yup.object().shape({
 
 function Register() {
   const dispatch = useAppDispatch();
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      phone: '',
-      password: '',
-    }, validationSchema,
-    onSubmit: async (values, helpers) => {
-      const res = await dispatch(createUser({ ...values, phone: Number(values.phone) }));
-      if (res.payload.error) {
-        console.log(res.payload);
-        helpers.setFieldError(res.payload.path, res.payload.msg);
-      }
-    },
-  });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input
-        placeholder="Enter your name"
-        type="text"
-        id="name"
-        name="name"
-        onBlur={formik.handleBlur}
-        onChange={formik.handleChange}
-        value={formik.values.name}
-      />
-      {formik.touched.name && formik.errors.name && <div className={styles.error}>{formik.errors.name}</div>}
-      <label htmlFor="email">Email</label>
-      <input
-        placeholder="Enter your email"
-        type="email"
-        id="email"
-        name="email"
-        onBlur={formik.handleBlur}
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email && <div className={styles.error}>{formik.errors.email}</div>}
-      <label htmlFor="phone">Phone Number</label>
-      <input
-        placeholder="Enter your phone number"
-        type="tel"
-        id="phone"
-        name="phone"
-        onBlur={formik.handleBlur}
-        onChange={formik.handleChange}
-        value={formik.values.phone}
-      />
-      {formik.touched.phone && formik.errors.phone && <div className={styles.error}>{formik.errors.phone}</div>}
-      <label htmlFor="password">Password</label>
-      <input
-        placeholder="Enter your password"
-        type="password"
-        id="password"
-        name="password"
-        onBlur={formik.handleBlur}
-        onChange={formik.handleChange}
-        value={formik.values.password}
-      />
-      {formik.touched.password && formik.errors.password && <div className={styles.error}>{formik.errors.password}</div>}
-      <Button disabled={Object.entries(formik.errors) ? false : true} type="submit" color='var(--default-blue)'>Sign In</Button>
-      <span>Do you have an account? <Link to='/'>Log In</Link></span>
-    </form>
+    <Formik
+      validationSchema={validationSchema}
+      initialValues={{
+        name: '',
+        email: '',
+        phone: '',
+        password: '',
+      }}
+      onSubmit={async (values, helpers) => {
+        const res = await dispatch(createUser({ ...values, phone: Number(values.phone) }));
+        if (res.payload.error) {
+          console.log(res.payload);
+          helpers.setFieldError(res.payload.path, res.payload.msg);
+        }
+      }}
+    >
+      {({ handleSubmit, isValid, isSubmitting }) => (
+        <form action="" onSubmit={handleSubmit}>
+          {isSubmitting && 'Kayıt olunuyor...'}
+          <label htmlFor="name">Name</label>
+          <Field id='name' name='name' component='input' placeholder="Enter your name" type="text" />
+          <ErrorMessage name="name" component='small' className={styles.error} />
+          <label htmlFor="email">Email</label>
+          <Field id='email' name='email' component='input' placeholder="Enter your name" type="email" />
+          <ErrorMessage name="email" component='small' className={styles.error} />
+          <label htmlFor="phone">Phone Number</label>
+          <Field id='phone' name='phone' component='input' placeholder="Enter your name" type="tel" />
+          <ErrorMessage name="phone" component='small' className={styles.error} />
+          <label htmlFor="password">Password</label>
+          <Field id='password' name='password' component='input' placeholder="Enter your name" type="password" />
+          <ErrorMessage name="password" component='small' className={styles.error} />
+          <Button disabled={!isValid} type="submit" color='var(--default-blue)'>Sign In</Button>
+          <span>Do you have an account? <Link to='/'>Log In</Link></span>
+        </form>
+      )}
+    </Formik>
   )
 }
 
