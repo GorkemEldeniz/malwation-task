5dc01d62d2e2adb8f51a0b837741f91db2f148ce
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2023-06-26 12:01:43 +0300

    eslint fixed

--- client/src/pages/Table/Table.tsx
+++ client/src/pages/Table/Table.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import styles from './table.module.css';
 import { useAppDispatch, useAppSelector } from '../../store/hook';
 import { useEffect, useMemo, useState } from 'react';
