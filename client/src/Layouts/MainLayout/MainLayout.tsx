8594e521038ee4a0a40b7e5b0d8796f345bbeb03
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2023-06-27 13:52:20 +0300

    loading component added

--- client/src/Layouts/MainLayout/MainLayout.tsx
+++ client/src/Layouts/MainLayout/MainLayout.tsx
@@ -1,8 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import AsideBar from '../../components/AsideBar';
 import styles from './mainlayout.module.css';
+import { useNavigation } from 'react-router-dom';
+import Loader from '../../components/Loader';
 
 function MainLayout() {
+
+  const state = useNavigation();
+
+  if (state.state == 'loading') return <div><Loader /></div>
+
   return (
     <main className={styles.main}>
       <AsideBar />
--- client/src/pages/User/User.tsx
+++ client/src/pages/User/User.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useLoaderData, useNavigate, defer, Await, useSubmit } from "react-router-dom"
+import { useLoaderData, useNavigate, useSubmit } from "react-router-dom"
 import { Formik, ErrorMessage, Field } from "formik"
 import Button from "../../components/Button"
 import styles from './user.module.css'
@@ -7,10 +7,7 @@ import * as yup from 'yup';
 import * as utils from '../../utils/index'
 import type { LoaderFunctionArgs } from "react-router-dom";
 import type { ActionFunctionArgs } from "react-router-dom";
-import { useState } from "react";
-import Loader from "../../components/Loader";
 import type { UserState } from "../../store/features/userSlice";
-import { Suspense } from "react";
 
 
 const validationSchema = yup.object().shape({
@@ -21,7 +18,7 @@ const validationSchema = yup.object().shape({
   active: yup.boolean()
 });
 
-const fetchUser = async (id: number): Promise<any | UserState> => {
+const fetchUser = async (id: string | undefined): Promise<any | UserState> => {
   const user = await fetch(`http://localhost:3000/user/${id}`, {
     headers: {
       'Content-Type': 'application/json'
@@ -34,13 +31,13 @@ const fetchUser = async (id: number): Promise<any | UserState> => {
 export const userLoader = async ({ params }: LoaderFunctionArgs) => {
   const { id } = params;
   const user = await fetchUser(id)
-  return defer({ user });
+  return user;
 }
 
 export const userAction = async ({ params, request }: ActionFunctionArgs) => {
   const { id } = params;
-  let payload = await request.formData();
-  payload = {
+  const payload = await request.formData();
+  const data = {
     phone: Number(payload.get('phone')), name: payload.get('name'),
     email: payload.get('email'), active: Boolean(payload.get('active')), role: payload.get('role')
   }
@@ -50,7 +47,7 @@ export const userAction = async ({ params, request }: ActionFunctionArgs) => {
       'Content-Type': 'application/json'
     },
     method: 'PUT',
-    body: JSON.stringify(payload)
+    body: JSON.stringify(data)
   });
   return user.json();
 }
@@ -58,103 +55,94 @@ export const userAction = async ({ params, request }: ActionFunctionArgs) => {
 
 function User() {
   const submit = useSubmit();
-  const { user } = useLoaderData() as UserState;
-
-  const [loading, setLoading] = useState(false);
+  const user = useLoaderData() as UserState;
   const navigator = useNavigate();
 
-  if (loading) return <div className="loader"><Loader /></div>
-
   return (
-    <Suspense fallback={<Loader />}>
-      <Await resolve={user}>
-        <Formik
-          initialValues={{
-            name: user.name,
-            email: user.email,
-            role: user.role,
-            phone: user.phone,
-            active: user.active,
-          }}
-          validationSchema={validationSchema}
-          onSubmit={async (values) => {
-            setLoading(true)
-            submit(values, {
-              method: 'POST'
-            })
-            setLoading(false);
-          }}
-        >
-          {({ isValid, handleSubmit, dirty }) => (
-            <section className={styles.update}>
-              <a onClick={(e) => {
-                e.preventDefault();
-                navigator(-1)
-              }}>Back to the User</a>
-              <header>
-                <h1>Update User</h1>
-              </header>
-              <form onSubmit={handleSubmit}>
-                <div>
-                  <label htmlFor="name">Name</label>
-                  <Field
-                    type="text"
-                    name="name"
-                    id="name"
-                    component='input'
-                  />
-                  <ErrorMessage name="name" />
-                </div>
+    <Formik
+      initialValues={{
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        phone: user.phone,
+        active: user.active,
+      }}
+      validationSchema={validationSchema}
+      onSubmit={async (values) => {
 
-                <div>
-                  <label htmlFor="email">Mail</label>
-                  <Field
-                    type="email"
-                    name="email"
-                    id="email"
-                    component='input'
-                  />
-                  <ErrorMessage name="email" />
-                </div>
+        submit(values, {
+          method: 'POST'
+        })
+      }}
+    >
+      {({ isValid, handleSubmit, dirty }) => (
+        <section className={styles.update}>
+          <a onClick={(e) => {
+            e.preventDefault();
+            navigator(-1)
+          }}>Back to the User</a>
+          <header>
+            <h1>Update User</h1>
+          </header>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Name</label>
+              <Field
+                type="text"
+                name="name"
+                id="name"
+                component='input'
+              />
+              <ErrorMessage name="name" />
+            </div>
 
-                <div>
-                  <label htmlFor="phone">Phone</label>
-                  <Field
-                    type="tel"
-                    name="phone"
-                    id="phone"
-                    component='input'
-                  />
-                  <ErrorMessage name="phone" />
-                </div>
+            <div>
+              <label htmlFor="email">Mail</label>
+              <Field
+                type="email"
+                name="email"
+                id="email"
+                component='input'
+              />
+              <ErrorMessage name="email" />
+            </div>
 
-                <div>
-                  <label htmlFor="role">Role</label>
-                  <Field name="role" id="role" component='select'>
-                    <option value="ADMIN">Admin</option>
-                    <option value="USER">User</option>
-                  </Field>
-                </div>
+            <div>
+              <label htmlFor="phone">Phone</label>
+              <Field
+                type="tel"
+                name="phone"
+                id="phone"
+                component='input'
+              />
+              <ErrorMessage name="phone" />
+            </div>
 
-                <div>
-                  <label htmlFor="active">Active</label>
-                  <label className={styles.switch} htmlFor="active">
-                    <Field
-                      type="checkbox"
-                      id="active"
-                      name="active"
-                      component='input'
-                    />
-                    <div className={`${styles.slider} ${styles.round}`}></div>
-                  </label>
-                </div>
-                <Button type="submit" disabled={!isValid || !dirty} color="var(--default-blue)">Update User</Button>
-              </form>
-            </section>
-          )}
-        </Formik>
-      </Await>
-    </Suspense>
+            <div>
+              <label htmlFor="role">Role</label>
+              <Field name="role" id="role" component='select'>
+                <option value="ADMIN">Admin</option>
+                <option value="USER">User</option>
+              </Field>
+            </div>
+
+            <div>
+              <label htmlFor="active">Active</label>
+              <label className={styles.switch} htmlFor="active">
+                <Field
+                  type="checkbox"
+                  id="active"
+                  name="active"
+                  component='input'
+                />
+                <div className={`${styles.slider} ${styles.round}`}></div>
+              </label>
+            </div>
+            <Button type="submit" disabled={!isValid || !dirty} color="var(--default-blue)">Update User</Button>
+          </form>
+        </section>
+      )}
+    </Formik>
   )
 
 }
